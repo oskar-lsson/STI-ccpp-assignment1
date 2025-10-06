39cf795d9a14afbcbca94cbb326e27d17f81dfed
OskarKarlsson <oskar-lsson@github.com>
2025-10-06 15:08:40 +0200

    add name change

--- sensorData/sensorData/sensorData.cpp
+++
@@ -1,9 +0,0 @@
-// sensorData.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    
-}
---
+++ sensorData/sensorData/sensorDataApp.cpp
@@ -0,0 +1,9 @@
+// sensorDataApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+}
